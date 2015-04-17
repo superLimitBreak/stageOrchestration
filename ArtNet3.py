cdb17c07eee2769856035482d3af4f430f4b357b
Allan Callaghan <allan.callaghan@thisisglobal.com>
2015-04-17 22:39:58 +0100

    ArtNet packet decoing example with test

--- ArtNet3.py
+++ ArtNet3.py
@@ -1,10 +1,13 @@
+## -*- coding: utf-8 -*-
+
 from struct import Struct
 from collections import namedtuple
 
 
 class DMXDatagram(object):
+    header_id = b'Art-Net\x00'
     opcodes = {
-        None: (namedtuple('Header', ['ID', 'OpCode', 'ProtVerHi', 'ProtVerLo']), Struct('8s<HBB')),
+        None: (namedtuple('Header', ['ID', 'OpCode', 'ProtVerHi', 'ProtVerLo']), Struct('>8sHBB')),
         0x2000: (namedtuple('Poll', []), ''),
         0x2100: (namedtuple('PollReply', []), ''),
         0x2300: (namedtuple('DiagData', []), ''),
@@ -40,3 +43,28 @@ class DMXDatagram(object):
         0x9a00: (namedtuple('Directory', []), ''),
         0x9b00: (namedtuple('DirectoryReply', []), ''),
     }
+
+    @staticmethod
+    def decode(raw_data):
+        r"""
+        >>> DMXDatagram.decode(b'Art-Net\x00\x97\x00\x01\x04\x00\x00\x18\x3C\x3C\x18\x00')
+        (TimeCode(Frames=24, Seconds=60, Minutes=60, Hours=24, Type=0), b'')
+        """
+        header_structure, header_struct = DMXDatagram.opcodes[None]
+        header_data = header_structure._make(header_struct.unpack(raw_data[0:header_struct.size]))
+        assert header_data.ID == DMXDatagram.header_id
+        data_structure, data_struct = DMXDatagram.opcodes[header_data.OpCode]
+        data = data_structure._make(data_struct.unpack(raw_data[header_struct.size:]))
+        return data, raw_data[header_struct.size + data_struct.size:]
+
+#import traceback
+#import pdb
+#import sys
+#try:
+#    print(DMXDatagram.decode(b'Art-Net\x00\x97\x00\x01\x04\x00\x00\x18\x3C\x3C\x18\x00'))
+#except:
+#    type, value, tb = sys.exc_info()
+#    traceback.print_exc()
+#    pdb.post_mortem(tb)
+
+
