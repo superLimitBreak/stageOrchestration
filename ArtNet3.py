418a22f246e2936168217ca8f9eaa9e4fb194419
Allan Callaghan <allan.callaghan@thisisglobal.com>
2015-04-23 08:46:47 +0100

    DMXDatagram - Considered singleton instance and inheritence to use this pattern for other protocols in future

--- ArtNet3.py
+++ ArtNet3.py
@@ -3,69 +3,121 @@
 from struct import Struct
 from collections import namedtuple
 
-OpCode = namedtuple('OpCode', ('name', 'opcode', 'fields', 'struct'))
-opcode_definitions = (
-    OpCode('Header', None, ('ID', 'OpCode', 'ProtVerHi', 'ProtVerLo'), '>8sHBB'),
-    OpCode('Poll', 0x2000, (), ''),
-    OpCode('PollReply', 0x2100, (), ''),
-    OpCode('DiagData', 0x2300, (), ''),
-    OpCode('Command', 0x2400, (), ''),
-    OpCode('Output', 0x5000, ('Sequence', 'Physical', 'SubUni', 'Net', 'LengthHi', 'Length', 'Data'), 'BBBBBB'),
-    OpCode('Nzs', 0x5100, (), ''),
-    OpCode('Address', 0x6000, (), ''),
-    OpCode('Input', 0x7000, (), ''),
-    OpCode('TodRequest', 0x8000, (), ''),
-    OpCode('TodData', 0x8100, (), ''),
-    OpCode('TodControl', 0x8200, (), ''),
-    OpCode('Rdm', 0x8300, (), ''),
-    OpCode('RdmSub', 0x8400, (), ''),
-    OpCode('VideoSetup', 0xa010, (), ''),
-    OpCode('VideoPalette', 0xa020, (), ''),
-    OpCode('VideoData', 0xa040, (), ''),
-    OpCode('MacMaster', 0xf000, (), ''),
-    OpCode('MacSlave', 0xf100, (), ''),
-    OpCode('FirmwareMaster', 0xf200, (), ''),
-    OpCode('FirmwareReply', 0xf300, (), ''),
-    OpCode('FileTnMaster', 0xf400, (), ''),
-    OpCode('FileFnMaster', 0xf500, (), ''),
-    OpCode('FileFnReply', 0xf600, (), ''),
-    OpCode('IpProg', 0xf800, (), ''),
-    OpCode('IpProgReply', 0xf900, (), ''),
-    OpCode('Media', 0x9000, (), ''),
-    OpCode('MediaPatch', 0x9100, (), ''),
-    OpCode('MediaControl', 0x9200, (), ''),
-    OpCode('MediaContrlReply', 0x9300, (), ''),
-    OpCode('TimeCode', 0x9700, ('Frames', 'Seconds', 'Minutes', 'Hours', 'Type'), 'xxBBBBB'),
-    OpCode('TimeSync', 0x9800, (), ''),
-    OpCode('Trigger', 0x9900, (), ''),
-    OpCode('Directory', 0x9a00, (), ''),
-    OpCode('DirectoryReply', 0x9b00, (), ''),
-)
 
-class DMXDatagram(object):
-    header_id = b'Art-Net\x00'
-    lookup_opcodes = {}
-    lookup_op = {}
+OpCodeDefinition = namedtuple('OpCodeDefinition', ('name', 'opcode', 'fields', 'struct'))
 
-    @staticmethod
-    def _init_opcodes(opcode_defenitions):
+
+class Datagram(object):
+
+    def __init__(self,  opcode_defenitions):
+        self.lookup_opcode = {}
+        self.lookup_namedtuple = {}
+        self.lookup_struct = {}
         for opcode in opcode_defenitions:
-            DMXDatagram.lookup_opcodes[opcode.opcode] = (namedtuple(opcode.name, opcode.fields), Struct(opcode.struct))  #(namedtuple('Header', ), Struct()),
-            #DMXDatagram.lookup_op[opcode.name] = opcode
+            opcode_namedtuple = namedtuple(opcode.name, opcode.fields)
+            self.lookup_opcode[opcode.opcode] = opcode_namedtuple
+            self.lookup_opcode[opcode_namedtuple] = opcode.opcode
+            self.lookup_namedtuple[opcode.name] = opcode_namedtuple
+            self.lookup_struct[opcode_namedtuple] = Struct(opcode.struct)
 
-    @staticmethod
-    def decode(raw_data):
+    def get_namedtuple(self, index):
+        if (isinstance(index, int)):
+            return self.lookup_opcode[index]
+        else:
+            return self.lookup_namedtuple[index]
+
+    def get_struct(self, opcode_namedtuple):
+        return self.lookup_struct[opcode_namedtuple]
+
+    def get_opcode(self, opcode_namedtuple):
+        return self.lookup_opcode[opcode_namedtuple]
+
+
+class DMXDatagram(Datagram):
+    header_id = b'Art-Net\x00'
+    header_ProtVerHi = 1
+    header_ProtVerLo = 4
+
+    opcode_definitions = (
+        OpCodeDefinition('Header', None, ('ID', 'OpCode', 'ProtVerHi', 'ProtVerLo'), '>8sHBB'),
+        OpCodeDefinition('Poll', 0x2000, (), ''),
+        OpCodeDefinition('PollReply', 0x2100, (), ''),
+        OpCodeDefinition('DiagData', 0x2300, (), ''),
+        OpCodeDefinition('Command', 0x2400, (), ''),
+        OpCodeDefinition('Output', 0x5000, ('Sequence', 'Physical', 'SubUni', 'Net', 'LengthHi', 'Length', 'Data'), 'BBBBBB'),
+        OpCodeDefinition('Nzs', 0x5100, (), ''),
+        OpCodeDefinition('Address', 0x6000, (), ''),
+        OpCodeDefinition('Input', 0x7000, (), ''),
+        OpCodeDefinition('TodRequest', 0x8000, (), ''),
+        OpCodeDefinition('TodData', 0x8100, (), ''),
+        OpCodeDefinition('TodControl', 0x8200, (), ''),
+        OpCodeDefinition('Rdm', 0x8300, (), ''),
+        OpCodeDefinition('RdmSub', 0x8400, (), ''),
+        OpCodeDefinition('VideoSetup', 0xa010, (), ''),
+        OpCodeDefinition('VideoPalette', 0xa020, (), ''),
+        OpCodeDefinition('VideoData', 0xa040, (), ''),
+        OpCodeDefinition('MacMaster', 0xf000, (), ''),
+        OpCodeDefinition('MacSlave', 0xf100, (), ''),
+        OpCodeDefinition('FirmwareMaster', 0xf200, (), ''),
+        OpCodeDefinition('FirmwareReply', 0xf300, (), ''),
+        OpCodeDefinition('FileTnMaster', 0xf400, (), ''),
+        OpCodeDefinition('FileFnMaster', 0xf500, (), ''),
+        OpCodeDefinition('FileFnReply', 0xf600, (), ''),
+        OpCodeDefinition('IpProg', 0xf800, (), ''),
+        OpCodeDefinition('IpProgReply', 0xf900, (), ''),
+        OpCodeDefinition('Media', 0x9000, (), ''),
+        OpCodeDefinition('MediaPatch', 0x9100, (), ''),
+        OpCodeDefinition('MediaControl', 0x9200, (), ''),
+        OpCodeDefinition('MediaContrlReply', 0x9300, (), ''),
+        OpCodeDefinition('TimeCode', 0x9700, ('Frames', 'Seconds', 'Minutes', 'Hours', 'Type'), 'xxBBBBB'),
+        OpCodeDefinition('TimeSync', 0x9800, (), ''),
+        OpCodeDefinition('Trigger', 0x9900, (), ''),
+        OpCodeDefinition('Directory', 0x9a00, (), ''),
+        OpCodeDefinition('DirectoryReply', 0x9b00, (), ''),
+    )
+
+    def __init__(self):
+        Datagram.__init__(self, DMXDatagram.opcode_definitions)
+
+    def decode(self, raw_data):
         r"""
-        >>> DMXDatagram._init_opcodes(opcode_definitions)
-        >>> DMXDatagram.decode(b'Art-Net\x00\x97\x00\x01\x04\x00\x00\x18\x3C\x3C\x18\x00')
+        >>> DMXDatagram().decode(b'Art-Net\x00\x97\x00\x01\x04\x00\x00\x18\x3C\x3C\x18\x00')
         (TimeCode(Frames=24, Seconds=60, Minutes=60, Hours=24, Type=0), b'')
         """
-        header_structure, header_struct = DMXDatagram.lookup_opcodes[None]
-        header_data = header_structure._make(header_struct.unpack(raw_data[0:header_struct.size]))
+        # Decode Header
+        header_namedtuple = self.get_namedtuple('Header')
+        header_struct = self.get_struct(header_namedtuple)
+        header_data = header_namedtuple._make(header_struct.unpack(raw_data[0:header_struct.size]))
+        # Check Header
         assert header_data.ID == DMXDatagram.header_id
-        data_structure, data_struct = DMXDatagram.lookup_opcodes[header_data.OpCode]
-        data = data_structure._make(data_struct.unpack(raw_data[header_struct.size:]))
+
+        # Decode Structured Data (now we know what opcode is being performed)
+        data_namedtuple = self.get_namedtuple(header_data.OpCode)
+        data_struct = self.get_struct(data_namedtuple)
+        data = data_namedtuple._make(data_struct.unpack(raw_data[header_struct.size:]))
+
         return data, raw_data[header_struct.size + data_struct.size:]
+
+    def encode(self, opcode_namedtuple_data, data=b''):
+        r"""
+        >>> DMXDatagram().encode(TimeCode(Frames=24, Seconds=60, Minutes=60, Hours=24, Type=0), b'')
+        b'Art-Net\x00\x97\x00\x01\x04\x00\x00\x18\x3C\x3C\x18\x00'
+        """
+        opcode = self.get_opcode(opcode_namedtuple_data.__class__)
+        data_struct = self.get_struct(opcode_namedtuple_data.__class__)
+
+        # Encode Header
+        header_namedtuple = self.get_namedtuple('Header')
+        header_struct = self.get_struct(header_namedtuple)
+        header_data = header_struct.pack(header_namedtuple(ID=DMXDatagram.header_id, OpCode=opcode, ))
+
+        
+        
+
+
+
+
+
 
 #import traceback
 #import pdb
