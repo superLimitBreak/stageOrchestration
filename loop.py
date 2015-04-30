22f7ed97ecba40876daa4ace03b050cb9446a159
Allan Callaghan <allan.callaghan@thisisglobal.com>
2015-04-30 08:02:13 +0100

    Considered loop accuracy. Loop now is aware of how long a cycle takes and can calculate the require sleep time. The system sleep time is not that accurate. Considered dividing the period by a constatnt to have have more sub time divisions. May need to consider timing accuraracy in the future

--- loop.py
+++ loop.py
@@ -9,31 +9,32 @@ class Loop(object):
     def set_period(self, fps):
         assert fps, "Provide fps"
         self.period = 1 / fps
-        self.previous_frame = 0
         self.start_time = time.time()
         self.previous_time = self.start_time
         return self.period
 
-    @property
-    def current_frame(self):
-        return int((self.current_time - self.start_time) // self.period)
+    def get_frame(self, timestamp):
+        return int((timestamp - self.start_time) // self.period)
 
     def loop(self):
         while self.period:
             self.current_time = time.time()
 
-            for frame_offset in range(self.current_frame - self.previous_frame):
-                self.render(self.current_frame + frame_offset)
-            self.previous_frame = self.current_frame
+            current_frame = self.get_frame(self.current_time)
+            previous_frame = self.get_frame(self.previous_time)
+            for frame_offset in range(current_frame - previous_frame):
+                self.render(current_frame + frame_offset)
 
-            self.pervious_time = self.current_time
+            self.previous_time = self.current_time
 
             sleep_time = (self.current_time + self.period) - time.time()
+            #sleep_time = self.period / 4
             if sleep_time > 0:
                 time.sleep(sleep_time)
 
     def render(self, frame):
-        print(frame)
+        pass
+        #print('{0} {1}'.format(frame, time.time()))
 
 
 def postmortem(func):
@@ -52,5 +53,3 @@ if __name__ == "__main__":
     #postmortem(lambda:
             Loop(10).loop()
     #)
-
-
