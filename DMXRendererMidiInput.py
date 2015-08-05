e9d1ef60d832a065afe8f199935ebefe8d0077d6
Allan Callaghan <calaldees@gmail.com>
2015-08-05 19:10:25 +0100

    Added hsv controls to light groups to inputs. God this is a hack but it'll do the job for now

--- DMXRendererMidiInput.py
+++ DMXRendererMidiInput.py
@@ -1,3 +1,5 @@
+from colorsys import hsv_to_rgb
+
 from libs.pygame_midi_input import MidiInput
 
 from DMXBase import AbstractDMXRenderer
@@ -8,6 +10,10 @@ log = logging.getLogger(__name__)
 
 class DMXRendererMidiInput(AbstractDMXRenderer):
     CONTROL_OFFSET_JUMP = 8
+    CONTROL_ID_HSV_A = -512  # todo - find correct values for inputs
+    CONTROL_ID_HSV_B = -512
+    HSV_A_INDEXS = [0, 8, 16, 24]
+    HSV_B_INDEXS = [32, 48, 56, 64]
 
     def __init__(self, name):
         super().__init__()
@@ -17,6 +23,8 @@ class DMXRendererMidiInput(AbstractDMXRenderer):
         self.midi_input.midi_event = self.midi_event  # Dynamic POWER!!!! Remap the midi event to be on this object!
 
         self._control_offset = 0
+        self.hsv_a = [0.0, 0.0, 0.0, 0.0]
+        self.hsv_b = [0.0, 0.0, 0.0, 0.0]
 
     def render(self, frame):
         self.midi_input.process_events()
@@ -33,6 +41,22 @@ class DMXRendererMidiInput(AbstractDMXRenderer):
             log.info('control_offset: {0}'.format(self.control_offset))
         if data1 >= 0 and data1 < self.CONTROL_OFFSET_JUMP:
             self.dmx_universe[self.control_offset + data1] = data2 * 2
+
+        def update_hsv_from_input(control_id, value, id_range, hsv_color):
+            if control_id in range(id_range, id_range + 4):
+                hsv_color[control_id - id_range] = value / 127
+        update_hsv_from_input(data1, data2, self.CONTROL_ID_HSV_A, self.hsv_a)
+        update_hsv_from_input(data1, data2, self.CONTROL_ID_HSV_B, self.hsv_b)
+
+        def color_float_to_byte(value):
+            return min(255, max(0, int(value * 255)))
+        def render_hsv_from_state(indexs, color):
+            for dmx_index in indexs:
+                for color_index, color_component in enumerate(hsv_to_rgb(*color[:3]) + color[3:4]):
+                    self.dmx_universe[dmx_index + color_index] = color_float_to_byte(color_component)
+        render_hsv_from_state(self.HSV_A_INDEXS, self.hsv_a)
+        render_hsv_from_state(self.HSV_B_INDEXS, self.hsv_b)
+
         #print('lights2 {0} {1} {2} {3}'.format(event, data1, data2, data3))
 
     def close(self):
