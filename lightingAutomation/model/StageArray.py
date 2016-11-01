cd171344e432f634aadeca913db2537402129c27
Allan Callaghan <calaldees@gmail.com>
2016-11-01 19:08:21 +0000

    Stubs for device array

--- lightingAutomation/model/StageArray.py
+++
@@ -1,8 +0,0 @@
-
-class StageArray(object):
-    """
-    A collection of lights with positions.
-    This models the stage layout and devices
-    """
-    def __init__(self):
-        pass
---
+++ lightingAutomation/model/device_array.py
@@ -0,0 +1,18 @@
+
+class DeviceArray(object):
+    """
+    A collection of lights with positions.
+    This models the stage layout and devices
+    """
+    def __init__(self):
+        pass
+
+    @property
+    def pack_size(self):
+        return sum()
+
+    def pack(self, buffer, offset):
+        pass
+
+    def unpack(self, buffer, offset):
+        pass
