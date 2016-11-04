dd05a492c01d31d36bdde05f29b647521c533e36
Allan Callaghan <calaldees@gmail.com>
2016-11-04 08:52:14 +0000

    Considered DeviceArray Added setter/getter aliass for strip lights

--- lightingAutomation/model/device_array.py
+++ lightingAutomation/model/device_array.py
@@ -1,10 +1,26 @@
-from ext.attribute_packer import CollectionPackerMixin
+from copy import copy
+from collections import OrderedDict
+
+from pysistence import make_dict
+
+from ext.attribute_packer import CollectionPackerMixin, BasePackerMixin
+
+from .devices.rgb_light import RGBLight
+from .devices.rgb_strip_light import RGBStripLight
+from .devices.effect_light import EffectRGBLight
+from .devices.smoke import Smoke
+from .devices.dmx_passthrough import DMXPassthru
 
 
-class DeviceArray(CollectionPackerMixin):
+class DeviceCollection(CollectionPackerMixin):
     """
     A collection of lights with positions.
     This models the stage layout and devices
     """
-    def __init__(self):
-        CollectionPackerMixin.__init__(self, xx)
+    def __init__(self, devices):
+        self._devices = make_dict(devices)
+        self.groups = dict()
+        CollectionPackerMixin.__init__(self, tuple(self._devices.values()))
+
+    def get(self, name):
+        pass
--- lightingAutomation/model/devices/rgb_strip_light.py
+++ lightingAutomation/model/devices/rgb_strip_light.py
@@ -6,3 +6,30 @@ class RGBStripLight(CollectionPackerMixin):
     def __init__(self, count):
         self.lights = tuple(RGBLight() for i in range(count))
         CollectionPackerMixin.__init__(self, self.lights)
+
+    def _average_group_attr(self, attr):
+        return sum(getattr(light, attr) for light in self.lights) / len(self.lights)
+    def _set_group_attr(self, attr, value):
+        for light in self.lights:
+            setattr(light, attr, value)
+
+    @property
+    def red(self):
+        return self._average_attr('red')
+    @red.setter
+    def _red_setter(self, value):
+        self._set_group_attr('red', value)
+
+    @property
+    def green(self):
+        return self._average_attr('green')
+    @green.setter
+    def _green_setter(self, value):
+        self._set_group_attr('green', value)
+
+    @property
+    def blue(self):
+        return self._average_attr('blue')
+    @blue.setter
+    def _blue_setter(self, value):
+        self._set_group_attr('blue', value)
