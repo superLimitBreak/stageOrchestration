ae6d36d903bd6a1f0284908aaecd65ac0bda8b8b
Allan Callaghan <calaldees@gmail.com>
2016-11-07 08:01:07 +0000

    Renamed device_collection DeviceCollection can now persist/restore state? Time tp write some tests

--- lightingAutomation/model/device_collection.py
+++ lightingAutomation/model/device_collection.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 from pysistence import make_dict
 
-from ext.attribute_packer import CollectionPackerMixin, BasePackerMixin
+from ext.attribute_packer import CollectionPackerMixin, BasePackerMixin, MemoryFramePacker
 
 from .devices.rgb_light import RGBLight
 from .devices.rgb_strip_light import RGBStripLight
@@ -13,12 +13,13 @@ from .devices.smoke import Smoke
 from .devices.dmx_passthrough import DMXPassthru
 
 
-class DeviceCollection(CollectionPackerMixin):
+class DeviceCollection(CollectionPackerMixin, MemoryFramePacker):
     """
     """
     def __init__(self, devices):
         self._devices = make_dict(devices)
         CollectionPackerMixin.__init__(self, tuple(self._devices.values()))
+        MemoryFramePacker.__init__(self, self)
         self._device_lookup = {device_name: (device, ) for device_name, device in self._devices.items()}
 
     def add_group(self, group_name, device_names):
