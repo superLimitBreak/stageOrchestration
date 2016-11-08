1fd145d89d86604d277f5c54d4928642fccf2667
Allan Callaghan <calaldees@gmail.com>
2016-11-08 19:07:23 +0000

    Revamping of commandline args

--- lightingServer.py
+++ lightingServer.py
@@ -28,17 +28,15 @@ def get_args():
         description=DESCRIPTION,
     )
 
-    # Core
     parser.add_argument('--config', action='store', help='', default=DEFAULT_CONFIG_FILENAME)
     parser.add_argument('--displaytrigger_host', action='store', help='display-trigger server to recieve events from')
-    parser.add_argument('-f', '--framerate', action='store', help='Frames per second', type=int)
+    parser.add_argument('--framerate', action='store', help='Frames per second', type=float)
     parser.add_argument('--artnet_dmx_host', action='store', help='ArtNet3 ip address')
-
-    # Plugin params
-    parser.add_argument('--yamlscaninterval', action='store', type=float, help='seconds to scan')
+    parser.add_argument('--scaninterval', action='store', type=float, help='seconds to scan datafiles for changes')
 
     parser.add_argument('--postmortem', action='store_true', help='Enter debugger on exception')
     parser.add_argument('--log_level', type=int, help='log level', default=logging.INFO)
+
     parser.add_argument('--version', action='version', version=VERSION)
 
     kwargs = vars(parser.parse_args())
@@ -46,7 +44,7 @@ def get_args():
     # Overlay config defaults from file
     with open(kwargs['config'], 'rt') as config_filehandle:
         config = yaml.load(config_filehandle)
-        kwargs = {k: v or config.get(k) for k, v in kwargs.items()}
+        kwargs = {k: v if v is not None else config.get(k) for k, v in kwargs.items()}
 
     return kwargs
 
