84bd7ebbbd9b16b3cddd1937cb8a296d59c6719d
Allan Callaghan <calaldees@gmail.com>
2017-05-19 08:09:02 +0100

    Experiment at driving two stagger sequences at once

--- lightingSequences/test.py
+++ lightingSequences/test.py
@@ -8,5 +8,8 @@ def create_timeline(dc, t):
     tl = Timeline()
     #tl.set_(dc.get_device('floorLarge1'), values={'red': 0, 'green': 0, 'blue': 0})
     #tl.from_to(dc.get_devices('sidesFloor'), t('16.0.0'), {'red': 1, 'green':0}, {'red': 0, 'green': 1})
-    tl.staggerTo(dc.get_device('floorLarge1').lights, duration=t('8.0.0'), values={'red': 0, 'green': 1}, item_delay=t('1.0.0'))
+    for rgb_strip_lights in (dc.get_device(device_name).lights for device_name in ('floorLarge1', 'floorLarge2')):
+        tl_intermediate = Timeline()
+        tl_intermediate.staggerTo(rgb_strip_lights, duration=t('4.0.0'), values={'red': 0, 'green': 1}, item_delay=t('1.0.0'))
+        tl &= tl_intermediate
     return tl
