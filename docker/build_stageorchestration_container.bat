c3b96296f924e4dd95759528370fb6e5b7b52ae0
Allan Callaghan <calaldees@gmail.com>
2018-07-03 17:51:58 +0100

    build docker container script keeps mainline python up to date

--- docker/build_stageorchestration_container.bat
+++ docker/build_stageorchestration_container.bat
@@ -1,4 +1,5 @@
 SET PATH_BUILD=..
+docker pull python:3
 docker build -t superlimitbreak/stageorchestration_base:latest --file .\stageOrchestration.base.dockerfile .
 docker build -t superlimitbreak/stageorchestration:latest --file .\stageOrchestration.production.dockerfile %PATH_BUILD%
 
