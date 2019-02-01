afd5da52aaae046c082a640e07ee907eb5a8f3b1
Allan Callaghan <calaldees@gmail.com>
2019-02-01 21:10:37 +0000

    First proof of concept for timing lighting models Tick tock light test I can't belive it ... it ... it ... it WORKS!!!! Everything I've theorised for 2 and a half years ... it's here .. it really is ... I'm fairly emotional now

--- data/sequences/test.py
+++ data/sequences/test.py
@@ -2,19 +2,39 @@ from calaldees.animation.timeline import Timeline
 
 META = {
     'name': 'Test of tests',
-    'bpm': 120,
+    'bpm': 138,
     'timesignature': '4:4',
 }
 
 def create_timeline(dc, t, tl, el):
     #tl.set_(dc.get_device('floorLarge1'), values={'red': 0, 'green': 0, 'blue': 0})
     #tl.from_to(dc.get_devices('sidesFloor'), t('16.0.0'), {'red': 1, 'green':0}, {'red': 0, 'green': 1})
-    for rgb_strip_lights in (dc.get_device(device_name).lights for device_name in ('floorLarge1', 'floorLarge2')):
+
+    devices = (dc.get_device('floorLarge1'), dc.get_device('floorLarge2'))
+
+    WHITE = {'red': 1, 'green': 1, 'blue': 1}
+    BLACK = {'red': 0, 'green': 0, 'blue': 0}
+    RED = {'red': 1, 'green': 0, 'blue': 0}
+    GREEN = {'red': 0, 'green': 1, 'blue': 0}
+    BLUE = {'red': 0, 'green': 0, 'blue': 1}
+    YELLOW = {'red': 1, 'green': 1, 'blue': 0}
+    CYAN = {'red': 0, 'green': 1, 'blue': 1}
+    MAGENTA = {'red': 1, 'green': 0, 'blue': 1}
+
+    def tick_tock(devices, colors, duration=t('1.2.1')):
         tl_intermediate = Timeline()
-        tl_intermediate.staggerTo(rgb_strip_lights, duration=t('4.1.1'), values={'red': 0, 'green': 1, 'blue': 0}, item_delay=t('2.1.1'))
-        tl_intermediate.set_(rgb_strip_lights, values={'red': 0, 'green': 0, 'blue': 0})
-        tl &= tl_intermediate
-    tl = tl * 4
+        for color in colors:
+            tl_intermediate.from_to(devices, valuesFrom=color, duration=duration)
+        return tl_intermediate
+
+    tl &= tick_tock(dc.get_devices('floorLarge1'), (WHITE, BLACK)) & tick_tock(dc.get_devices('floorLarge2'), (RED, YELLOW))
+
+    # for rgb_strip_lights in (dc.get_device(device_name).lights for device_name in ('floorLarge1', 'floorLarge2')):
+    #     tl_intermediate = Timeline()
+    #     tl_intermediate.staggerTo(rgb_strip_lights, duration=t('4.1.1'), values={'red': 0, 'green': 1, 'blue': 0}, item_delay=t('1.1.2'))
+    #     tl_intermediate.set_(rgb_strip_lights, values={'red': 0, 'green': 0, 'blue': 0})
+    #     tl &= tl_intermediate
+    tl = tl * 32
 
     el.add_trigger({
         "deviceid": "audio",
