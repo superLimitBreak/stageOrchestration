4a9f6ff9bdc5b0dd7d7a489bee31e0c7f5bbc44c
Allan Callaghan <calaldees@gmail.com>
2019-07-29 07:55:16 +0100

    Added test for file changed event

--- tests/test_server.py
+++ tests/test_server.py
@@ -18,8 +18,20 @@ def test_network_event__load_sequence(mocker):
     MockSequenceManager.get_triggerline.assert_called_with('test')
 
 
-def test_scan_update_event():
-    pass
+def test_scan_update_event(mocker):
+    MockSequenceManager = mocker.patch('stageOrchestration.server.SequenceManager')
+    MockSubscriptionClient = mocker.patch('stageOrchestration.server.StageOrchestrationServer.NullSubscriptionClient')()
+    server = StageOrchestrationServer()
+
+    server.scan_update_event(())
+
+    MockSubscriptionClient.send_message.assert_called_with({
+        'deviceid': 'light_visulisation',
+        'func': 'scan_update_event',
+        'sequence_files': (),
+        'sequence_module_name': '',
+        'module_hash': '',
+    })
 
 
 def test_frame_event(mocker):
